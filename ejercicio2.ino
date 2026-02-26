8454d3610ea41e50087e1ce020d1e9fc5b0e3171
ale_aguilar01 <aguilarsolorzanoalexandra@gmail.com>
2026-02-25 21:58:29 -0300

    ejercicio2.ino

--- ejercicio2.ino
+++ ejercicio2.ino
@@ -4,30 +4,38 @@
 int lista[MAX_SIZE];
 int N;
 
-void cargarRandom() {
+void cargarListaRandom(int *lista) {
   Serial.println("Ingrese tamaño de la lista:");
   while (Serial.available() == 0);
   N = Serial.parseInt();
 
   if (N <= 0 || N > MAX_SIZE) {
-    Serial.println("Tamaño invalido");
+    Serial.println("Tamaño inválido");
     return;
   }
 
   for (int i = 0; i < N; i++) {
     lista[i] = random(1, 21);
-    Serial.println(lista[i]);
   }
+
+  Serial.println("Lista generada aleatoriamente.");
+}
+
+void mostrarLista(int *lista) {
+  Serial.println("Contenido de la lista:");
+  for (int i = 0; i < N; i++)
+    Serial.println(lista[i]);
 }
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
   randomSeed(analogRead(A0));
-  Serial.println("EJERCICIO 2 - CARGA RANDOM");
+  Serial.println("EJERCICIO 2 - LISTA RANDOM");
 }
 
 void loop() {
-  cargarRandom();
+  cargarListaRandom(lista);
+  mostrarLista(lista);
   while (true);
 }
